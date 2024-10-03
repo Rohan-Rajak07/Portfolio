c3a86242a9a456e8ee247ce4614b33f16c57d81f
Rohan-Rajak07 <rohanrajak132@gmail.com>
2024-10-03 19:42:05 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 import * as THREE from 'three';
-
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
 
+// export default defineConfig({
+//   base: '/Portfolio/',
+// });
 export default defineConfig({
-  base: '/Portfolio/',
-});
+    base: '/Portfolio/',
+    build: {
+        chunkSizeWarningLimit: 1000 // Set to 1000 kB, or any size you're comfortable with
+      }
+  });
