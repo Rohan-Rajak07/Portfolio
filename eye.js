5eba6774e838f148acae7da58aef964e9035c692
Rohan-Rajak07 <rohanrajak132@gmail.com>
2024-10-12 12:05:44 +0530

    Fix AutoWrite

--- eye.js
+++ eye.js
@@ -6,7 +6,7 @@
 //Import the THREE.js library
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 // To allow for the camera to move around the scene
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+// import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 // To allow for importing the .gltf file
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 import {AnimationAction} from 'three';
